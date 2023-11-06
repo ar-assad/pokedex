c34e11f7b7e2daf5ddafde871ae18eaedc9019aa
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-11-07 00:25:31 +0500

    Create sprite component

---
+++ src/components/Sprite.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import getPokemonData from '../getPokemonData';
+
+export default function Sprite({ name }) {
+  const [spriteUrl, setSpriteUrl] = useState('');
+  useEffect(() => {
+    const data = getPokemonData(name);
+    setSpriteUrl(data.sprites.front_default);
+  }, [name]);
+  if (spriteUrl) {
+    return <img src={spriteUrl} className="sprite"/>;
+  }
+  else {
+    return null;
+  }
+
+}
