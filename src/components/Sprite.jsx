1979570f73a37e2bcf14eabaf45ba44c88b60329
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-11-07 00:48:47 +0500

    Format code

--- src/components/Sprite.jsx
+++ src/components/Sprite.jsx
@@ -1,17 +1,15 @@
-import { useState, useEffect } from 'react';
-import getPokemonData from '../getPokemonData';
+import { useState, useEffect } from "react";
+import getPokemonData from "../getPokemonData";
 
 export default function Sprite({ name }) {
-  const [spriteUrl, setSpriteUrl] = useState('');
+  const [spriteUrl, setSpriteUrl] = useState("");
   useEffect(() => {
     const data = getPokemonData(name);
     setSpriteUrl(data.sprites.front_default);
   }, [name]);
   if (spriteUrl) {
-    return <img src={spriteUrl} className="sprite"/>;
-  }
-  else {
+    return <img src={spriteUrl} className="sprite" />;
+  } else {
     return null;
   }
-
 }
