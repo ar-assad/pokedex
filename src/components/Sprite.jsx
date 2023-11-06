c5efc585c3ec446c6810e6721e327a9d2e559e57
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-11-07 01:33:37 +0500

    Add fetchSprite function to sprite component
    The getPokemonData module was deleted since it returned a promise
    anyways and it was reduntant to use another async/await block each time
    it was to be called. Call the API inside the component.

--- src/components/Sprite.jsx
+++ src/components/Sprite.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react";
-import getPokemonData from "../getPokemonData";
+import axios from "axios";
 
 export default function Sprite({ name }) {
   const [spriteUrl, setSpriteUrl] = useState("");
   useEffect(() => {
-    const data = getPokemonData(name);
-    setSpriteUrl(data.sprites.front_default);
+    async function fetchSprite() {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${name}`
+        );
+        setSpriteUrl(response.data.sprites.front_default);
+      } catch (error) {
+        console.error("Error fetching pokemon sprite: ", error);
+      }
+    }
+    fetchSprite();
   }, [name]);
   if (spriteUrl) {
     return <img src={spriteUrl} className="sprite" />;
