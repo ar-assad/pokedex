cc9ed895a6e9b096f899043eac9f95f1e8f6fb55
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-11-08 03:04:46 +0500

    Add types

--- src/components/info_components/PokeCard.jsx
+++ src/components/info_components/PokeCard.jsx
@@ -1,16 +1,23 @@
-import PropTypes from 'prop-types';
-import Sprite from '../Sprite';
+import PropTypes from "prop-types";
+import Sprite from "../Sprite";
 
-export default function PokeCard({ data }){
-    return (
-        <div className="card-container">
-            <p className="dexNo">No. {data.id}</p>
-            <Sprite name={data.name} />
-            <p className="name">{data.name}</p>
-        </div>
-    )
+export default function PokeCard({ data }) {
+  return (
+    <div className="card-container">
+      <p className="dexNo">No. {data.id}</p>
+      <Sprite name={data.name} />
+      <p className="name">{data.name}</p>
+      <ul>
+        {data.types.map((item, index) => (
+          <li key={index} className={item.type.name}>
+            {item.type.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 PokeCard.propTypes = {
-    data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired,
+};
