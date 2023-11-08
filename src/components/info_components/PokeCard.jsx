e0c979182bd877244219b1ed13c8609d089c2bdf
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-11-08 11:50:20 +0500

    Add buttons for displaying different sprite versions

--- src/components/info_components/PokeCard.jsx
+++ src/components/info_components/PokeCard.jsx
@@ -1,11 +1,40 @@
 import PropTypes from "prop-types";
 import Sprite from "../Sprite";
+import { useState } from "react";
 
 export default function PokeCard({ data }) {
+  const [spriteUrl, setSpriteUrl] = useState(data.sprites.front_default);
+  console.log(spriteUrl);
+  const spriteUrls = {
+    "*": data.sprites.front_shiny,
+    I: data.sprites.versions["generation-i"]["red-blue"].front_default,
+    II: data.sprites.versions["generation-ii"].crystal.front_default,
+    III: data.sprites.versions["generation-iii"].emerald.front_default,
+    IV: data.sprites.versions["generation-iv"].platinum.front_default,
+    V: data.sprites.versions["generation-v"]["black-white"].animated
+      .front_default,
+    VI: data.sprites.versions["generation-vi"]["x-y"].front_default,
+    VII: data.sprites.versions["generation-vii"]["ultra-sun-ultra-moon"]
+      .front_default,
+    O: data.sprites.other["official-artwork"].front_default,
+  };
+
   return (
     <div className="card-container">
-      <p className="dexNo">No. {data.id}</p>
-      <Sprite name={data.name} />
+      <div className="top-bar">
+        <p className="dexNo((">No. {data.id}</p>
+      </div>
+      <Sprite name={data.name} url={spriteUrl} />
+      <ul>
+        {Object.entries(spriteUrls).map(
+          ([version, url], index) =>
+            url && (
+              <li key={index}>
+                <button onClick={() => setSpriteUrl(url)}>{version}</button>
+              </li>
+            )
+        )}
+      </ul>
       <p className="name">{data.name}</p>
       <ul>
         {data.types.map((item, index) => (
