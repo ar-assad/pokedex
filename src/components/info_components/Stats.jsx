a6fb5268d5a8293c087d40cdba94dd439c4cdc81
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-11-09 15:06:41 +0500

    Style stat container

--- src/components/info_components/Stats.jsx
+++ src/components/info_components/Stats.jsx
@@ -1,27 +1,49 @@
 import PropTypes from "prop-types";
+import "./Stats.css";
 
 export default function Stats({ data }) {
   return (
-    <div className="main">
+    <div className="stats-container">
       <div className="abilities">
         <p>Abilities</p>
         <ul>
-          {data.abilities.map((item) => (
-            <li key={item.ability.name}>{item.ability.name}</li>
-          ))}
+          {data.abilities.map((item) => {
+            if (item.is_hidden)
+              return (
+                <li key={item.ability.name} className="hidden">
+                  {item.ability.name}
+                </li>
+              );
+            return <li key={item.ability.name}>{item.ability.name}</li>;
+          })}
         </ul>
       </div>
-      <div className="stats">
-      <p>Stats</p>
-        {data.stats.map((item) => (
-            <li key={item.stat.name}>{item.stat.name} {item.base_stat}</li>
-        ))}
+      <div className="battle-stats">
+        <p>Stats</p>
+        <ul>
+          {data.stats.map((item) => (
+            <li key={item.stat.name} className={item.stat.name}>
+              <p>
+                {item.stat.name === "special-attack"
+                  ? "sp.atk"
+                  : item.stat.name === "special-defense"
+                  ? "sp.def"
+                  : item.stat.name}
+              </p>
+              <p>{item.base_stat}</p>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="index">
         <p>Index</p>
         <ul>
-            <li key="height">Height: {data.height / 10} m</li>
-            <li key="weight">Weight: {data.weight / 10} kg</li>
+          <li key="height">
+            <p>Height</p> <p>{data.height * 10} cm</p>
+          </li>
+          <li key="weight">
+            <p>Weight</p> <p>{data.weight / 10} kg</p>
+          </li>
         </ul>
       </div>
     </div>
